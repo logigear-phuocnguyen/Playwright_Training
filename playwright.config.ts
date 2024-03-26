49843bbe1251eaa100d63eeb15bb3fdd488caf65
Phuoc Nguyen <phuoc.van.nguyen@logigear.com>
2024-03-26 12:16:00 +0900

    Run test on Action

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:54000',
+    baseURL: 'http://192.168.1.250:54000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
